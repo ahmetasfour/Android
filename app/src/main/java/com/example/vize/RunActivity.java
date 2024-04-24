3d5cb9f113cbb615518dd09eba29b66f5a449a8c
Moha-decv <135824876+Moha-decv@users.noreply.github.com>
2024-04-25 01:10:43 +0300

    1

--- app/src/main/java/com/example/vize/RunActivity.java
+++ app/src/main/java/com/example/vize/RunActivity.java
@@ -16,12 +16,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
@@ -32,18 +26,13 @@ import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.firebase.firestore.DocumentSnapshot;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+
 public class RunActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private MapView mapView;
-    private GoogleMap gMap;
     private Chronometer chronometer;
     private long pauseOffset;
     private boolean running;
@@ -51,144 +40,42 @@ public class RunActivity extends AppCompatActivity implements OnMapReadyCallback
     private Location lastLocation;
     private float totalDistance = 0;
     private TextView distanceTextView;
+    private MapView mapView;
+    private GoogleMap gMap;
 
     private FirestoreService firestoreService;
-    private Location startLocation;
-    private Location pauseLocation;
-
     private TextView dailyRunsText;
 
-/*    @Override
+    private LocationCallback locationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            for (Location location : locationResult.getLocations()) {
+                if (lastLocation != null) {
+                    float distance = lastLocation.distanceTo(location);
+                    totalDistance += distance;
+                    updateDistanceDisplay();
+                }
+                lastLocation = location;
+            }
+        }
+    };
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_run);
 
+        chronometer = findViewById(R.id.chronometer);
+        distanceTextView = findViewById(R.id.distanceTextView);
+        dailyRunsText = findViewById(R.id.day1Distance);
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
 
-        chronometer = findViewById(R.id.chronometer);
-        distanceTextView = findViewById(R.id.distanceTextView);
-
         firestoreService = new FirestoreService();
         locationClient = LocationServices.getFusedLocationProviderClient(this);
+
         updateDailyRunsDisplay();
-    }*/
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_run);
-
-    mapView = findViewById(R.id.mapView);
-    mapView.onCreate(savedInstanceState);
-    mapView.getMapAsync(this);
-
-    chronometer = findViewById(R.id.chronometer);
-    distanceTextView = findViewById(R.id.distanceTextView);
-    // Initialize dailyRunsText with the correct ID from your layout
-    dailyRunsText = findViewById(R.id.day1Distance);
-
-    firestoreService = new FirestoreService();
-    locationClient = LocationServices.getFusedLocationProviderClient(this);
-
-    // It's important to call updateDailyRunsDisplay after initializing dailyRunsText
-    updateDailyRunsDisplay();
-}
-
-    public void startChronometer(View view) {
-        if (!running) {
-            chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
-            chronometer.start();
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                startLocationUpdates();  // Start location updates when the chronometer starts
-                locationClient.getLastLocation()
-                        .addOnSuccessListener(location -> {
-                            if (location != null) {
-                                startLocation = location;
-                                lastLocation = location; // Initialize lastLocation with the starting point
-                            }
-                        });
-            } else {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 101);
-            }
-            running = true;
-        }
-    }
-
-
-    public void pauseChronometer(View view) {
-        if (running) {
-            chronometer.stop();
-            pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
-            // Capture the stop location and calculate the distance
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                locationClient.getLastLocation()
-                        .addOnSuccessListener(location -> {
-                            if (location != null) {
-                                pauseLocation = location;
-                                calculateDistance();
-                            }
-                        });
-            }
-            running = false;
-        }
-    }
-
-    public void resetChronometer(View view) {
-        chronometer.stop();
-        chronometer.setBase(SystemClock.elapsedRealtime());
-        pauseOffset = 0;
-        totalDistance = 0;
-        updateDistanceDisplay();
-        lastLocation = null;
-        startLocation = null;
-        pauseLocation = null;
-        running = false;
-    }
-
-    private void calculateDistance() {
-        if (startLocation != null && pauseLocation != null) {
-            String origin = startLocation.getLatitude() + "," + startLocation.getLongitude();
-            String destination = pauseLocation.getLatitude() + "," + pauseLocation.getLongitude();
-            String apiKey = "AIzaSyAQ3I21JOwxLFQOIcZ-rVafBPLw-By5Cqk"; // Replace with your API key
-
-            String url = "https://maps.googleapis.com/maps/api/directions/json?origin=" + origin + "&destination=" + destination + "&key=" + apiKey;
-
-            JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
-                    response -> {
-                        try {
-                            JSONArray routes = response.getJSONArray("routes");
-                            JSONObject route = routes.getJSONObject(0);
-                            JSONArray legs = route.getJSONArray("legs");
-                            JSONObject leg = legs.getJSONObject(0);
-                            JSONObject distance = leg.getJSONObject("distance");
-                            String distanceText = distance.getString("text");
-
-                            // Display the distanceText on your UI
-                            distanceTextView.setText("Distance: " + distanceText);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }, error -> Log.e(TAG, "Error getting directions: " + error.getMessage()));
-
-            // Add the request to the RequestQueue
-            RequestQueue queue = Volley.newRequestQueue(this);
-            queue.add(request);
-        }
-    }
-
-    private void updateDistanceDisplay() {
-        distanceTextView.setText(String.format(Locale.getDefault(), "Distance: %.2f m", totalDistance));
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
-        String date = dateFormat.format(new Date());
-        firestoreService.addDailyRun(date, totalDistance, task -> {
-            if (task.isSuccessful()) {
-                updateDailyRunsDisplay();
-            } else {
-                // Log or handle the error
-                Log.e(TAG, "Error adding run to Firestore");
-            }
-        });
     }
 
     @Override
@@ -206,39 +93,71 @@ protected void onCreate(Bundle savedInstanceState) {
         }
     }
 
-    private void startLocationUpdates() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            LocationRequest locationRequest = LocationRequest.create();
-            locationRequest.setInterval(5000);
-            locationRequest.setFastestInterval(2000);
-            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-            locationRequest.setSmallestDisplacement(10); // Set displacement in meters
-
-            locationClient.requestLocationUpdates(locationRequest, locationCallback, null);
+    public void startChronometer(View view) {
+        if (!running) {
+            chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
+            chronometer.start();
+            startLocationUpdates();
+            running = true;
         }
     }
 
-    private LocationCallback locationCallback = new LocationCallback() {
-        @Override
-        public void onLocationResult(LocationResult locationResult) {
-            if (locationResult != null) {
+    public void pauseChronometer(View view) {
+        if (running) {
+            chronometer.stop();
+            pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
+            running = false;
+        }
+    }
+
+    public void resetChronometer(View view) {
+        chronometer.stop();
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        pauseOffset = 0;
+        totalDistance = 0;
+        updateDistanceDisplay();
+        lastLocation = null;
+        running = false;
+    }
+
+    private void startLocationUpdates() {
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setInterval(2000);
+        locationRequest.setFastestInterval(1000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setSmallestDisplacement(5);
+
+        LocationCallback locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
                 for (Location location : locationResult.getLocations()) {
-                    Log.d("Location Update", "Location: " + location.getLatitude() + ", " + location.getLongitude() + " Accuracy: " + location.getAccuracy());
-                    if (lastLocation != null && location.getAccuracy() < 5) {
+                    if (lastLocation != null) {
                         float distance = lastLocation.distanceTo(location);
-                        if (distance > 5) {
-                            totalDistance += distance;
-                            updateDistanceDisplay();
-                        }
+                        totalDistance += distance;
+                        updateDistanceDisplay();
                     }
                     lastLocation = location;
                 }
             }
-        }
-    };
+        };
 
-    private void stopLocationUpdates() {
-        locationClient.removeLocationUpdates(locationCallback);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationClient.requestLocationUpdates(locationRequest, locationCallback, null);
+        }
+    }
+
+    private void updateDistanceDisplay() {
+        distanceTextView.setText(String.format(Locale.getDefault(), "Distance: %.2f m", totalDistance));
+        // Log run to Firestore
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+        String date = dateFormat.format(new Date());
+        firestoreService.addDailyRun(date, totalDistance, task -> {
+            if (task.isSuccessful()) {
+                updateDailyRunsDisplay();
+            } else {
+                Log.e(TAG, "Error adding run to Firestore");
+            }
+        });
     }
 
     private void updateDailyRunsDisplay() {
@@ -257,7 +176,6 @@ protected void onCreate(Bundle savedInstanceState) {
                 }
             } else {
                 dailyRunsText.setText("Failed to load runs.");
-                Log.e("RunActivity", "Error getting documents: ", task.getException());
             }
         });
     }
@@ -291,5 +209,9 @@ protected void onCreate(Bundle savedInstanceState) {
     public void onLowMemory() {
         super.onLowMemory();
         mapView.onLowMemory();
+    }
+
+    private void stopLocationUpdates() {
+        locationClient.removeLocationUpdates(locationCallback);
     }
 }
